1016d1e4c5605e7db507a93000fd2a3f8d086dbd
Emily-Waters <emilyabigailwaters@gmail.com>
2024-10-15 15:34:11 -0700

    remove unused variables transformer

--- src/utilities/transformers/variables.ts
+++
@@ -1,6 +0,0 @@
-import { GraphQLType } from "graphql";
-import { BaseTransformer } from "./base";
-
-export class VariablesTransformer extends BaseTransformer {
-  transform(type: GraphQLType) {}
-}
